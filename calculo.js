763af5f21f4a6c1a513c8f64ab2272ecb7915243
Alex Daniel C.F <alexdaniel.adcf@gmail.com>
2022-09-20 22:32:33 -0400

    Versão 1.1 Calculo IMC

--- calculo.js
+++ calculo.js
@@ -21,10 +21,10 @@ function calcularIMC() {
  else {
     let imc = (peso / ((altura*altura)/10000)).toFixed(2);
 
-    if(imc <18.6) result.innerHTML='Abaixo do peso';
+    if(imc <18.6) result.innerHTML='Esta abaixo do peso indicado';
 
-    else if (imc >= 18.6 && imc < 24.9)result.innerHTML = 'Normal';
+    else if (imc >= 18.6 && imc < 24.9)result.innerHTML = 'Esta com o peso normal';
 
-    else result.innerHTML = 'Sobrepeso';
+    else result.innerHTML = 'Esta com Sobrepeso';
  }
 }
